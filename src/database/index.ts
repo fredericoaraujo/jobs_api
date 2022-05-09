7b95e02210e095fa837a7403f07ed24b0969cf15
Frederico Araujo <fredericoaraujo@gmail.com>
2022-05-08 22:05:28 -0300

    Created migrations and models candidate

---
+++ src/database/index.ts
@@ -0,0 +1,10 @@
+import { Sequelize } from "sequelize"
+
+const dbUrl = process.env.DATABASE_URL || ''
+const sequelize = new Sequelize(dbUrl, {
+    define: {
+        underscored: true
+    }
+})
+
+export { sequelize }
